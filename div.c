6b21423a8e7c12a42e3e43d0b3d883ef700335e0
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-10-19 10:05:50 +0000

    advance tasks 1.0.0

--- div.c
+++ div.c
@@ -1,9 +1,10 @@
 #include "monty.h"
+
 /**
  * _div - divides the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
 void _div(stack_t **head, unsigned int counter)
 {
@@ -37,4 +38,118 @@ void _div(stack_t **head, unsigned int counter)
 	h->next->n = aux;
 	*head = h->next;
 	free(h);
+}
+
+/**
+ * _mul - multiplies the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: void
+*/
+void _mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(opc.file);
+		free(opc.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n * h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+  *_sub- sustration
+  *@head: stack head
+  *@counter: line_number
+  *Return: void
+ */
+void _sub(stack_t **head, unsigned int counter)
+{
+	stack_t *aux;
+	int sus, nodes;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(opc.file);
+		free(opc.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
+}
+
+/**
+ * _mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: void
+*/
+void _mod(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(opc.file);
+		free(opc.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(opc.file);
+		free(opc.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+/**
+  *_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: void
+ */
+void _nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
 }
