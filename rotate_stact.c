07c8910bd69ea0f0dd0574a7f92f36c705bbce7e
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-10-19 14:11:15 +0100

    Update rotate_stact.c

--- rotate_stact.c
+++ rotate_stact.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
   *_rotl- rotates the stack to the top
   *@head: stack head
@@ -23,4 +24,30 @@ void _rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 	(*head)->next = NULL;
 	(*head)->prev = tmp;
 	(*head) = aux;
+}
+
+/**
+  *_rotr- rotates the stack to the bottom
+  *@head: stack head
+  *@counter: line_number
+  *Return: void
+ */
+void _rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
 }
